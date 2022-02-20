6b6446dc43aabd1c7b00fa037b2597be68aab7cb
Boluwatife Porter <bolup123@gm$ git config --global user.name FirstName>
2022-02-20 22:32:51 +0100

    Implemented entities biodata and linked identities (not proper objects yet)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BioDataRegistModule } from './bio-data-regist/bio-data-regist.module';
+import { BioDataModule } from './bio-data/bio-data.module';
+import { LinkedIdentityModule } from './linked-identity/linked-identity.module';
 
 @Module({
-  imports: [],
+  imports: [BioDataRegistModule, BioDataModule, LinkedIdentityModule],
   controllers: [AppController],
   providers: [AppService],
 })
